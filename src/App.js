933b75893a2a6ee7d194e9635eba8ca75b7c3a17
Daniel Holzknecht <78915530+Leyonad@users.noreply.github.com>
2023-08-04 11:58:22 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import { Button, ActionList, ButtonGroup, Box, PageLayout, ThemeProvider } from '@primer/react'
+import { ActionList, Box, PageLayout, PageHeader, ThemeProvider } from '@primer/react'
 import { Section } from './components/containerComponents.js';
 import PrimaryButton from './components/interactionComponents.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { TitleSection } from './components/sectionComponents';
 import "./primer.css"
+import GlobalHeader from './components/globalComponents';
 
 function Layout() {
 
   const LT_Icon = <FontAwesomeIcon icon={faCoffee} />
 
   return <>
+    <GlobalHeader />
     <PageLayout>
       <PageLayout.Header>
+        <Header>
+          <Header.Item>Item 1</Header.Item>
+          <Header.Item full border={1} borderStyle="solid">
+            Item 2
+          </Header.Item>
+          <Header.Item sx={{ mr: 0 }}>Item 3</Header.Item>
+        </Header>
         <TitleSection title="Temperatur" subtitle="19:20" />
         <FontAwesomeIcon icon={["fab", "github"]} />
       </PageLayout.Header>
